2bc404f27ea8224177ce8a53b921f96bcb160afa
leloufadel <leloufadel@gmail.com>
2023-08-10 21:56:18 +0300

    add all functionality of Financial

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -24,7 +24,7 @@ const Detail = () => {
   const loading = useSelector((state) => state.company.loading);
   const headline = 'Company Statement';
   const back = <i className="bx bx-chevron-left-circle" />;
-  const showlineBase = 'Company statement report for ';
+  const showlineBase = 'Financial statement Report(s)  |  Area: USA (+1) |\n  Code: ';
   const showline = showlineBase.concat(symbol);
 
   if (loading) {
@@ -36,59 +36,64 @@ const Detail = () => {
     <>
       <Header back={back} headline={headline} />
       <Showcase image={statementImage} showline={showline} />
+
       {report.length === 0 ? (
         <p className={css.noReports}>
-          No Reports
+          No Reports - INVALID CODE !!!
         </p>
-      ) : report.map((report, index) => (
-        <div key={`${report.researchExpenses}${report.date}`} className={css.cover}>
-          <h3 className={css.reportName}>
-            Report
-            {' '}
-            {index + 1}
-          </h3>
-          <p>
-            <span> Report Date: </span>
-            <span>
-              {' '}
-              {report.date}
-              <i className="bx bx-chevron-right-circle" />
-            </span>
-          </p>
-          <p>
-            <span> Currency:</span>
-            <span>
-              {' '}
-              {report.currency}
-              <i className="bx bx-chevron-right-circle" />
-            </span>
-          </p>
-          <p>
-            <span> Year:</span>
-            <span>
-              {' '}
-              {report.year}
-              <i className="bx bx-chevron-right-circle" />
-            </span>
-          </p>
-          <p>
-            <span> Expenses:</span>
-            <span>
-              {' '}
-              {report.researchExpenses}
-              <i className="bx bx-chevron-right-circle" />
-            </span>
-          </p>
-          <p>
-            <span> Income: </span>
-            <span>
-              {' '}
-              {report.income}
-              <i className="bx bx-chevron-right-circle" />
-            </span>
-          </p>
+      ) : (
+        <div className={css.reports}>
+          {report.map((report) => (
+            <div key={`${report.researchExpenses}${report.date}`} className={css.cover}>
+              <h3 className={css.reportName}>
+                Financial Statement
+                {' '}
+                {report.year}
+              </h3>
+              <p>
+                <span> Report Date: </span>
+                <span>
+                  {' '}
+                  {report.date}
+                  <i className="bx bx-chevron-right-circle" />
+                </span>
+              </p>
+              <p>
+                <span> Currency:</span>
+                <span>
+                  {' '}
+                  {report.currency}
+                  <i className="bx bx-chevron-right-circle" />
+                </span>
+              </p>
+              <p>
+                <span> Year:</span>
+                <span>
+                  {' '}
+                  {report.year}
+                  <i className="bx bx-chevron-right-circle" />
+                </span>
+              </p>
+              <p>
+                <span> Expenses:</span>
+                <span>
+                  {' '}
+                  {report.researchExpenses}
+                  <i className="bx bx-chevron-right-circle" />
+                </span>
+              </p>
+              <p>
+                <span> Income: </span>
+                <span>
+                  {' '}
+                  {report.income}
+                  <i className="bx bx-chevron-right-circle" />
+                </span>
+              </p>
+            </div>
+          ))}
         </div>
-      ))}
+      )}
 
     </>
   );
